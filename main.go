bb0ee6ca1c40ef83c246a94887972d44a34bfb4c
kyluke <1087345+kyluke@users.noreply.github.com>
2017-08-04 21:26:28 +0200

    Initial commit of the UI for the kyco.bitcoin.currency.tickers project

---
+++ main.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"database/sql"
+	// "fmt"
+	tm "github.com/buger/goterm"
+	"github.com/fsnotify/fsnotify"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/op/go-logging"
+	"github.com/spf13/viper"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var err error
+
+// Configure main config struct
+type Config struct {
+	Interval int
+	Log      string
+	SQLite   string
+}
+
+// Exchange data
+type ExchangeData struct {
+	Exchange     string
+	Price        float64
+	Ask          float64
+	Bid          float64
+	Volume       float64
+	Date         string
+	CurrencyCode string
+}
+
+var config Config
+var logFile *os.File
+var log = logging.MustGetLogger("bitcoin-ui")
+
+// Example format string. Everything except the message has a custom color
+// which is dependent on the log level. Many fields have a custom output
+// formatting too, eg. the time returns the hour down to the milli second.
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+// Setup global home and config folders
+// File location
+var fileLocation string = "/.config/bitcoin-ui/"
+var bitoinStatsLocation string = "/.config/bitcoin-stats/"
+
+// Get home folder
+var home string = os.Getenv("HOME") + fileLocation
+var sqliteDBLocation string = os.Getenv("HOME") + bitoinStatsLocation
+
+// manages the UI
+func bitcoin_prices() {
+
+	// Tick on the minute
+	// t := minuteTicker()
+
+	for {
+
+		tm.Clear() // Clear the screen
+		// wait for the tick
+		// <-t.C
+
+		begin := 3
+		// Underline
+		underline := strings.Repeat("-", 99)
+
+		// By moving cursor to top-left position we ensure that console output
+		// will be overwritten each time, instead of adding new.
+		tm.MoveCursor(begin, 40)
+
+		tm.Println("Current Time:", time.Now().Format(time.RFC1123))
+
+		tm.MoveCursor(begin+3, 60)
+		tm.Println(tm.Bold("1 Hour"))
+		tm.MoveCursorForward(10)
+		tm.Println(underline)
+
+		tm.MoveCursor(begin+5, 11)
+		tm.Printf(tm.Bold("Exchange"))
+		tm.MoveCursorForward(10)
+		tm.Printf(tm.Bold("Average"))
+		tm.MoveCursorForward(10)
+		tm.Printf(tm.Bold("Ask"))
+		tm.MoveCursorForward(13)
+		tm.Printf(tm.Bold("Buy"))
+		tm.MoveCursorForward(12)
+		tm.Printf(tm.Bold("Volume"))
+		tm.MoveCursorForward(8)
+		tm.Printf(tm.Bold("Time"))
+
+		// Reset to the beginning of the line
+		tm.MoveCursor(begin+6, 10)
+
+		// Grab SQLite data
+		get_last_exchange_rates("1")
+
+		tm.MoveCursorForward(60)
+		tm.MoveCursorDown(2)
+		tm.Println(tm.Bold("24 Hour"))
+		tm.MoveCursorForward(10)
+		tm.Println(underline)
+
+		tm.MoveCursorForward(10)
+		tm.Printf(tm.Bold("Exchange"))
+		tm.MoveCursorForward(10)
+		tm.Printf(tm.Bold("Average"))
+		tm.MoveCursorForward(10)
+		tm.Printf(tm.Bold("Ask"))
+		tm.MoveCursorForward(13)
+		tm.Printf(tm.Bold("Buy"))
+		tm.MoveCursorForward(12)
+		tm.Printf(tm.Bold("Volume"))
+		tm.MoveCursorForward(8)
+		tm.Printf(tm.Bold("Time"))
+
+		// Reset to the beginning of the line
+		tm.MoveCursorDown(1)
+
+		// Grab SQLite data
+		get_last_exchange_rates("24")
+
+		time.Sleep(20 * time.Second)
+
+	}
+
+}
+
+// Grab sqlite data from database
+func get_last_exchange_rates(hours string) {
+	// Open SQLite
+	db := sqlite_open()
+	result, err := db.Query(`select exchange, printf("%.2f", AVG(ask)) as ask, printf("%.2f", AVG(bid)) as bid,
+				printf("%.2f", AVG((ask + bid) / 2)) as price,
+				AVG(volume) as volume, datetime(timestamp, 'unixepoch') as timestamp, currencyCode
+				from exchanges
+				where (currencyCode = "USD" or currencyCode = "btcusd" or currencyCode = "btc_usd")
+					and datetime(timestamp, 'unixepoch') >= datetime('now', '-` + hours + ` Hour')
+				group by exchange
+				order by volume desc;`)
+
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+
+	// Loop through records
+	for result.Next() {
+		// Scan records into a struct
+		tmpStruct := &ExchangeData{}
+		result.Scan(&tmpStruct.Exchange, &tmpStruct.Ask, &tmpStruct.Bid, &tmpStruct.Price, &tmpStruct.Volume,
+			&tmpStruct.Date, &tmpStruct.CurrencyCode)
+
+		tm.Println()
+
+		tm.MoveCursorForward(10)
+		tm.Printf(tmpStruct.Exchange)
+		// tm.MoveCursorDown(1)
+		tm.MoveCursorForward((18 - len(tmpStruct.Exchange)))
+		tm.Printf(strconv.FormatFloat(tmpStruct.Price, 'f', 2, 64))
+		tm.Printf(" " + strings.ToUpper(strings.Replace(strings.Replace(tmpStruct.CurrencyCode, "btc", "", -1), "_", "", -1)))
+		// tm.MoveCursorDown(1)
+		tm.MoveCursorForward((13 - len(strconv.FormatFloat(tmpStruct.Price, 'f', 2, 64))))
+		tm.Printf(strconv.FormatFloat(tmpStruct.Ask, 'f', 2, 64))
+		tm.Printf(" " + strings.ToUpper(strings.Replace(strings.Replace(tmpStruct.CurrencyCode, "btc", "", -1), "_", "", -1)))
+		// tm.MoveCursorDown(1)
+		tm.MoveCursorForward((12 - len(strconv.FormatFloat(tmpStruct.Ask, 'f', 2, 64))))
+		tm.Printf(strconv.FormatFloat(tmpStruct.Bid, 'f', 2, 64))
+		tm.Printf(" " + strings.ToUpper(strings.Replace(strings.Replace(tmpStruct.CurrencyCode, "btc", "", -1), "_", "", -1)))
+		// tm.MoveCursorDown(1)
+		tm.MoveCursorForward((11 - len(strconv.FormatFloat(tmpStruct.Bid, 'f', 2, 64))))
+		tm.Printf(strconv.FormatFloat(tmpStruct.Volume, 'f', 2, 64))
+		// tm.MoveCursorDown(1)
+		tm.MoveCursorForward((14 - len(strconv.FormatFloat(tmpStruct.Volume, 'f', 2, 64))))
+		tm.Printf(tmpStruct.Date)
+
+	}
+
+	tm.Flush() // Call it every time at the end of rendering
+
+}
+
+// Open SQlite Connection
+func sqlite_open() *sql.DB {
+	db, err := sql.Open("sqlite3", sqliteDBLocation+"data.db")
+	if err != nil {
+		log.Error(err.Error())
+	}
+	return db
+}
+
+// Waits for the minute to tick over
+func minuteTicker() *time.Ticker {
+	// Return new ticker that triggers on the minute
+	return time.NewTicker(time.Second * time.Duration(int(60*config.Interval)-time.Now().Second()))
+}
+
+// Configure logging
+func config_log() {
+
+	// Check if it is already open
+	logFile.Close()
+
+	// Configure logging
+	logFile, err := os.OpenFile(config.Log, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		log.Info("error opening file: %v", err)
+	}
+
+	// For demo purposes, create two backend for os.Stderr.
+	loggingFile := logging.NewLogBackend(logFile, "", 0)
+
+	// For messages written to loggingFile we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	loggingFileFormatter := logging.NewBackendFormatter(loggingFile, format)
+
+	// Set the backends to be used.
+	logging.SetBackend(loggingFileFormatter)
+}
+
+// Configure configs
+func config_init() {
+
+	// Config File
+	viper.SetConfigName("config") // no need to include file extension
+	viper.AddConfigPath(home)     // set the path of your config file
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Info("Config file not found... Error %s\n", err)
+	} else {
+
+		// ========= CONFIG ================================================================
+		interval := viper.GetInt("interval")
+		logLocation := viper.GetString("log")
+		sqliteLocation := viper.GetString("sqliteLocation")
+		// Main Config
+		config = Config{
+			Interval: interval,
+			Log:      logLocation,
+			SQLite:   sqliteLocation,
+		}
+	}
+
+	// Monitor the config file for changes and reload
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+
+		// Re-configure config
+		config_init()
+
+		// Re-configure logging
+		config_log()
+
+		log.Info("Config file changed:", e.Name)
+	})
+}
+
+func main() {
+	// Initialise config file and settings
+	config_init()
+
+	// Configure logging
+	config_log()
+
+	// don't forget to close the log file
+	defer logFile.Close()
+
+	// Start UI process
+	go bitcoin_prices()
+
+	// Notify log that we are up and running
+	log.Info("started Bitcoin UI")
+
+	select {}
+}
---
+++ vendor/github.com/Beldur/kraken-go-api-client/types.go
@@ -0,0 +1,381 @@
+package krakenapi
+
+const (
+	DASHEUR  = "DASHEUR"
+	DASHUSD  = "DASHUSD"
+	DASHXBT  = "DASHXBT"
+	EOSETH   = "EOSETH"
+	EOSEUR   = "EOSEUR"
+	EOSUSD   = "EOSUSD"
+	EOSXBT   = "EOSXBT"
+	GNOETH   = "GNOETH"
+	GNOEUR   = "GNOEUR"
+	GNOUSD   = "GNOUSD"
+	GNOXBT   = "GNOXBT"
+	USDTZUSD = "USDTZUSD"
+	XETCXETH = "XETCXETH"
+	XETCXXBT = "XETCXXBT"
+	XETCZEUR = "XETCZEUR"
+	XETCXUSD = "XETCXUSD"
+	XETHXXBT = "XETHXXBT"
+	XETHZCAD = "XETHZCAD"
+	XETHZEUR = "XETHZEUR"
+	XETHZGBP = "XETHZGBP"
+	XETHZJPY = "XETHZJPY"
+	XETHZUSD = "XETHZUSD"
+	XICNXETH = "XICNXETH"
+	XICNXXBT = "XICNXXBT"
+	XLTCXXBT = "XLTCXXBT"
+	XLTCZEUR = "XLTCZEUR"
+	XLTCZUSD = "XLTCZUSD"
+	XMLNXETH = "XMLNXETH"
+	XMLNXXBT = "XMLNXXBT"
+	XREPXETH = "XREPXETH"
+	XREPXXBT = "XREPXXBT"
+	XREPZEUR = "XREPZEUR"
+	XREPZUSD = "XREPZUSD"
+	XXBTZCAD = "XXBTZCAD"
+	XXBTZEUR = "XXBTZEUR"
+	XXBTZGBP = "XXBTZGBP"
+	XXBTZJPY = "XXBTZJPY"
+	XXBTZUSD = "XXBTZUSD"
+	XXDGXXBT = "XXDGXXBT"
+	XXLMXXBT = "XXLMXXBT"
+	XXLMZEUR = "XXLMZEUR"
+	XXLMZUSD = "XXLMZUSD"
+	XXMRXXBT = "XXMRXXBT"
+	XXMRZEUR = "XXMRZEUR"
+	XXMRZUSD = "XXMRZUSD"
+	XXRPXXBT = "XXRPXXBT"
+	XXRPZCAD = "XXRPZCAD"
+	XXRPZEUR = "XXRPZEUR"
+	XXRPZJPY = "XXRPZJPY"
+	XXRPZUSD = "XXRPZUSD"
+	XZECXXBT = "XZECXXBT"
+	XZECZEUR = "XZECZEUR"
+	XZECZUSD = "XZECZUSD"
+)
+
+const (
+	BUY    = "b"
+	SELL   = "s"
+	MARKET = "m"
+	LIMIT  = "l"
+)
+
+// KrakenResponse wraps the Kraken API JSON response
+type KrakenResponse struct {
+	Error  []string    `json:"error"`
+	Result interface{} `json:"result"`
+}
+
+// TimeResponse represents the server's time
+type TimeResponse struct {
+	// Unix timestamp
+	Unixtime int64
+	// RFC 1123 time format
+	Rfc1123 string
+}
+
+// AssetPairsResponse includes asset pair informations
+type AssetPairsResponse struct {
+	DASHEUR  AssetPairInfo
+	DASHUSD  AssetPairInfo
+	DASHXBT  AssetPairInfo
+	EOSETH   AssetPairInfo
+	EOSEUR   AssetPairInfo
+	EOSUSD   AssetPairInfo
+	EOSXBT   AssetPairInfo
+	GNOETH   AssetPairInfo
+	GNOEUR   AssetPairInfo
+	GNOUSD   AssetPairInfo
+	GNOXBT   AssetPairInfo
+	USDTZUSD AssetPairInfo
+	XETCXETH AssetPairInfo
+	XETCXXBT AssetPairInfo
+	XETCZEUR AssetPairInfo
+	XETCXUSD AssetPairInfo
+	XETHXXBT AssetPairInfo
+	XETHZCAD AssetPairInfo
+	XETHZEUR AssetPairInfo
+	XETHZGBP AssetPairInfo
+	XETHZJPY AssetPairInfo
+	XETHZUSD AssetPairInfo
+	XICNXETH AssetPairInfo
+	XICNXXBT AssetPairInfo
+	XLTCXXBT AssetPairInfo
+	XLTCZEUR AssetPairInfo
+	XLTCZUSD AssetPairInfo
+	XMLNXETH AssetPairInfo
+	XMLNXXBT AssetPairInfo
+	XREPXETH AssetPairInfo
+	XREPXXBT AssetPairInfo
+	XREPZEUR AssetPairInfo
+	XREPZUSD AssetPairInfo
+	XXBTZCAD AssetPairInfo
+	XXBTZEUR AssetPairInfo
+	XXBTZGBP AssetPairInfo
+	XXBTZJPY AssetPairInfo
+	XXBTZUSD AssetPairInfo
+	XXDGXXBT AssetPairInfo
+	XXLMXXBT AssetPairInfo
+	XXLMZEUR AssetPairInfo
+	XXLMZUSD AssetPairInfo
+	XXMRXXBT AssetPairInfo
+	XXMRZEUR AssetPairInfo
+	XXMRZUSD AssetPairInfo
+	XXRPXXBT AssetPairInfo
+	XXRPZCAD AssetPairInfo
+	XXRPZEUR AssetPairInfo
+	XXRPZJPY AssetPairInfo
+	XXRPZUSD AssetPairInfo
+	XZECXXBT AssetPairInfo
+	XZECZEUR AssetPairInfo
+	XZECZUSD AssetPairInfo
+}
+
+// AssetPairInfo represents asset pair information
+type AssetPairInfo struct {
+	// Alternate pair name
+	Altname string `json:"altname"`
+	// Asset class of base component
+	AssetClassBase string `json:"aclass_base"`
+	// Asset id of base component
+	Base string `json:"base"`
+	// Asset class of quote component
+	AssetClassQuote string `json:"aclass_quote"`
+	// Asset id of quote component
+	Quote string `json:"quote"`
+	// Volume lot size
+	Lot string `json:"lot"`
+	// Scaling decimal places for pair
+	PairDecimals int `json:"pair_decimals"`
+	// Scaling decimal places for volume
+	LotDecimals int `json:"lot_decimals"`
+	// Amount to multiply lot volume by to get currency volume
+	LotMultiplier int `json:"lot_multiplier"`
+	// Array of leverage amounts available when buying
+	LeverageBuy []float32 `json:"leverage_buy"`
+	// Array of leverage amounts available when selling
+	LeverageSell []float32 `json:"leverage_sell"`
+	// Fee schedule array in [volume, percent fee] tuples
+	Fees [][]float64 `json:"fees"`
+	// // Maker fee schedule array in [volume, percent fee] tuples (if on maker/taker)
+	FeesMaker [][]float64 `json:"fees_maker"`
+	// // Volume discount currency
+	FeeVolumeCurrency string `json:"fee_volume_currency"`
+	// Margin call level
+	MarginCall int `json:"margin_call"`
+	// Stop-out/Liquidation margin level
+	MarginStop int `json:"margin_stop"`
+}
+
+// AssetsResponse includes asset informations
+type AssetsResponse struct {
+	DASH AssetInfo
+	EOS  AssetInfo
+	GNO  AssetInfo
+	KFEE AssetInfo
+	USDT AssetInfo
+	XDAO AssetInfo
+	XETC AssetInfo
+	XETH AssetInfo
+	XICN AssetInfo
+	XLTC AssetInfo
+	XMLN AssetInfo
+	XNMC AssetInfo
+	XREP AssetInfo
+	XXBT AssetInfo
+	XXDG AssetInfo
+	XXLM AssetInfo
+	XXMR AssetInfo
+	XXRP AssetInfo
+	XXVN AssetInfo
+	XZEC AssetInfo
+	ZCAD AssetInfo
+	ZEUR AssetInfo
+	ZGBP AssetInfo
+	ZJPY AssetInfo
+	ZKRW AssetInfo
+	ZUSD AssetInfo
+}
+
+// AssetInfo represents an asset information
+type AssetInfo struct {
+	// Alternate name
+	Altname string
+	// Asset class
+	AssetClass string `json:"aclass"`
+	// Scaling decimal places for record keeping
+	Decimals int
+	// Scaling decimal places for output display
+	DisplayDecimals int `json:"display_decimals"`
+}
+
+type BalanceResponse struct {
+	DASH float32 `json:"DASH,string"`
+	EOS  float32 `json:"EOS,string"`
+	GNO  float32 `json:"GNO,string"`
+	KFEE float32 `json:"KFEE,string"`
+	USDT float32 `json:"USDT,string"`
+	XDAO float32 `json:"XDAO,string"`
+	XETC float32 `json:"XETC,string"`
+	XETH float32 `json:"XETH,string"`
+	XICN float32 `json:"XICN,string"`
+	XLTC float32 `json:"XLTC,string"`
+	XMLN float32 `json:"XMLN,string"`
+	XNMC float32 `json:"XNMC,string"`
+	XREP float32 `json:"XREP,string"`
+	XXBT float32 `json:"XXBT,string"`
+	XXDG float32 `json:"XXDG,string"`
+	XXLM float32 `json:"XXLM,string"`
+	XXMR float32 `json:"XXMR,string"`
+	XXRP float32 `json:"XXRP,string"`
+	XXVN float32 `json:"XXVN,string"`
+	XZEC float32 `json:"XZEC,string"`
+	ZCAD float32 `json:"ZCAD,string"`
+	ZEUR float32 `json:"ZEUR,string"`
+	ZGBP float32 `json:"ZGBP,string"`
+	ZJPY float32 `json:"ZJPY,string"`
+	ZKRW float32 `json:"ZKRW,string"`
+	ZUSD float32 `json:"ZUSD,string"`
+}
+
+// TickerResponse includes the requested ticker pairs
+type TickerResponse struct {
+	DASHEUR  PairTickerInfo
+	DASHUSD  PairTickerInfo
+	DASHXBT  PairTickerInfo
+	EOSETH   PairTickerInfo
+	EOSEUR   PairTickerInfo
+	EOSUSD   PairTickerInfo
+	EOSXBT   PairTickerInfo
+	GNOETH   PairTickerInfo
+	GNOEUR   PairTickerInfo
+	GNOUSD   PairTickerInfo
+	GNOXBT   PairTickerInfo
+	USDTZUSD PairTickerInfo
+	XETCXETH PairTickerInfo
+	XETCXXBT PairTickerInfo
+	XETCZEUR PairTickerInfo
+	XETCXUSD PairTickerInfo
+	XETHXXBT PairTickerInfo
+	XETHZCAD PairTickerInfo
+	XETHZEUR PairTickerInfo
+	XETHZGBP PairTickerInfo
+	XETHZJPY PairTickerInfo
+	XETHZUSD PairTickerInfo
+	XICNXETH PairTickerInfo
+	XICNXXBT PairTickerInfo
+	XLTCXXBT PairTickerInfo
+	XLTCZEUR PairTickerInfo
+	XLTCZUSD PairTickerInfo
+	XMLNXETH PairTickerInfo
+	XMLNXXBT PairTickerInfo
+	XREPXETH PairTickerInfo
+	XREPXXBT PairTickerInfo
+	XREPZEUR PairTickerInfo
+	XREPZUSD PairTickerInfo
+	XXBTZCAD PairTickerInfo
+	XXBTZEUR PairTickerInfo
+	XXBTZGBP PairTickerInfo
+	XXBTZJPY PairTickerInfo
+	XXBTZUSD PairTickerInfo
+	XXDGXXBT PairTickerInfo
+	XXLMXXBT PairTickerInfo
+	XXLMZEUR PairTickerInfo
+	XXLMZUSD PairTickerInfo
+	XXMRXXBT PairTickerInfo
+	XXMRZEUR PairTickerInfo
+	XXMRZUSD PairTickerInfo
+	XXRPXXBT PairTickerInfo
+	XXRPZCAD PairTickerInfo
+	XXRPZEUR PairTickerInfo
+	XXRPZJPY PairTickerInfo
+	XXRPZUSD PairTickerInfo
+	XZECXXBT PairTickerInfo
+	XZECZEUR PairTickerInfo
+	XZECZUSD PairTickerInfo
+}
+
+// PairTickerInfo represents ticker information for a pair
+type PairTickerInfo struct {
+	// Ask array(<price>, <whole lot volume>, <lot volume>)
+	Ask []string `json:"a"`
+	// Bid array(<price>, <whole lot volume>, <lot volume>)
+	Bid []string `json:"b"`
+	// Last trade closed array(<price>, <lot volume>)
+	Close []string `json:"c"`
+	// Volume array(<today>, <last 24 hours>)
+	Volume []string `json:"v"`
+	// Volume weighted average price array(<today>, <last 24 hours>)
+	VolumeAveragePrice []string `json:"p"`
+	// Number of trades array(<today>, <last 24 hours>)
+	Trades []int `json:"t"`
+	// Low array(<today>, <last 24 hours>)
+	Low []string `json:"l"`
+	// High array(<today>, <last 24 hours>)
+	High []string `json:"h"`
+	// Today's opening price
+	OpeningPrice float32 `json:"o,string"`
+}
+
+// TradesResponse represents a list of the last trades
+type TradesResponse struct {
+	Last   int64
+	Trades []TradeInfo
+}
+
+// TradeInfo represents a trades information
+type TradeInfo struct {
+	Price         string
+	PriceFloat    float64
+	Volume        string
+	VolumeFloat   float64
+	Time          int64
+	Buy           bool
+	Sell          bool
+	Market        bool
+	Limit         bool
+	Miscellaneous string
+}
+
+// OrderDescription represents an orders description
+type OrderDescription struct {
+	AssetPair      string  `json:"pair"`
+	Close          string  `json:"close"`
+	Leverage       string  `json:"leverage"`
+	Order          string  `json:"order"`
+	OrderType      string  `json:"ordertype"`
+	PrimaryPrice   float64 `json:"price,string"`
+	SecondaryPrice float64 `json:"price2,string"`
+	Type           string  `json:"type"`
+}
+
+// Order represents a single order
+type Order struct {
+	ReferenceID    string           `json:"refid"`
+	UserRef        string           `json:"userref"`
+	Status         string           `json:"status"`
+	OpenTime       float64          `json:"opentm"`
+	StartTime      float64          `json:"starttm"`
+	ExpireTime     float64          `json:"expiretm"`
+	Description    OrderDescription `json:"descr"`
+	Volume         string           `json:"vol1"`
+	VolumeExecuted float64          `json:"vol_exec,string"`
+	Cost           float64          `json:"cost,string"`
+	Fee            string           `json:"fee"`
+	Price          float64          `json:"price,string"`
+	StopPrice      float64          `json:"stopprice"`
+	LimitPrice     float64          `json:"limitprice"`
+	Misc           string           `json:"misc"`
+	OrderFlags     string           `json:"oflags"`
+	CloseTime      float64          `json:"closetm"`
+	Reason         string           `json:"reason"`
+}
+
+// ClosedOrdersResponse represents a list of closed orders, indexed by id
+type ClosedOrdersResponse struct {
+	Closed map[string]Order `json:"closed"`
+	Count  int              `json:"count"`
+}
